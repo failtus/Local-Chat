db48e404d23e806ba3407138d7c3e57878ee6913
Failtus <iamfailtus@gmail.com>
2017-09-12 18:07:38 +0600

    Added facebook login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { Facebook } from '@ionic-native/facebook';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAr8Zty0up0QIQ6-O48Y9w6may3FEoLMe0",
+  authDomain: "com-local-chat.firebaseapp.com",
+  databaseURL: "https://com-local-chat.firebaseio.com",
+  projectId: "com-local-chat",
+  storageBucket: "com-local-chat.appspot.com",
+  messagingSenderId: "309215726872"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +41,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Facebook
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Platform, NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+
+
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +13,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items: FirebaseListObservable<any[]>; // test it for fetch data from database
+  displayName;
+  loggedUserImg;
 
+  constructor(public navCtrl: NavController, afDB: AngularFireDatabase, private afAuth: AngularFireAuth, private fb: Facebook, private platform: Platform) {
+    this.items = afDB.list('/cuisines'); // test it for fetch data from database
+    afAuth.authState.subscribe(user => {
+      if (!user) {
+        this.displayName = null;
+        return;
+      }
+      this.displayName = user.displayName;
+      this.loggedUserImg = user.photoURL;
+    });
+  }
+
+  signInWithFacebook() {
+    if (this.platform.is('cordova')) {
+      return this.fb.login(['email', 'public_profile']).then(res => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        return firebase.auth().signInWithCredential(facebookCredential);
+      })
+    }
+    else {
+      return this.afAuth.auth
+        .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+        .then(res => console.log(res));
+    }
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
   }
 
 }
